2988b2c63d0a34f8b1104bf8c2bb4a59f3e8cc36
DhruvRana1995 <129316407+DhruvRana1995@users.noreply.github.com>
2023-05-23 18:25:59 -0400

    Update backend_config.tf

--- infra/backend_config.tf
+++ infra/backend_config.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "test-cloud-devops-terraform-state-backend"
+    bucket         = "test-tf-state-backend-345w45t"
     key            = "terraform.tfstate"
     region         = "ap-south-1"
-    dynamodb_table = "terraform_state_db"
+    dynamodb_table = "tf_state_db"
     encrypt        = true
   }
 }
