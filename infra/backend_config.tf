77457642745bca9745067fd7191de3fb38157fca
DhruvRana1995 <dhruvrana.cilans@gmail.com>
2023-05-24 13:11:05 -0400

    Adding backend changes.

--- infra/backend_config.tf
+++ infra/backend_config.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "test-tf-state-backend-837c5yw"
+    bucket         = "test-tf-state-backend-wr7f6w7"
     key            = "terraform.tfstate"
     region         = "ap-south-1"
-    dynamodb_table = "tf_state_db"
+    dynamodb_table = "tf_state_db_wr7f6w7"
     encrypt        = true
   }
 }
